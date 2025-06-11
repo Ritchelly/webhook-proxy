44ec76d6078c7143f393f8c6a21fe9d79f5309a4
ritchelly <ritchelly@coffee-code.tech>
2025-06-11 15:39:15 -0300

    feat: add response.

--- webhook-proxy.php
+++ webhook-proxy.php
@@ -1,29 +1,26 @@
 <?php
 /*
 Plugin Name: Webhook proxy
-Description: Encaminha requisições do webhook da para um endpoint local via ngrok.
+Description: Encaminha requisições de um webhook para um endpoint local via ngrok.
 Version: 1.0
 Author: CoffeeCode
 */
 if (!defined('ABSPATH')) exit;
 
-// Adiciona página no admin
 add_action('admin_menu', function () {
     add_options_page(
-        'Webhook proxy',
-        'Webhook proxy',
+        'Getnet Webhook Forwarder',
+        'Getnet Webhook Forwarder',
         'manage_options',
-        'webhook-proxy',
+        'getnet-webhook-forwarder',
         'gwf_render_settings_page'
     );
 });
 
-// Registra a configuração
 add_action('admin_init', function () {
     register_setting('gwf_settings_group', 'gwf_ngrok_url');
 });
 
-// Renderiza a interface de configuração
 function gwf_render_settings_page() {
     ?>
     <div class="wrap">
@@ -46,41 +43,51 @@ function gwf_render_settings_page() {
     <?php
 }
 
-// Intercepta a requisição da Getnet
 add_action('init', function () {
-    // Detecta se a URL é /wc-api/getnet-creditcard
     if (stripos($_SERVER['REQUEST_URI'], '/wc-api/getnet') !== false) {
         gwf_forward_request();
     }
 }, 0);
 
-// Encaminha a requisição para o ngrok
 function gwf_forward_request() {
     $ngrok_url = rtrim(get_option('gwf_ngrok_url'), '/');
     if (!$ngrok_url) return;
 
     $target_url = $ngrok_url . $_SERVER['REQUEST_URI'];
 
-    // Pega o corpo da requisição original
     $body = file_get_contents('php://input');
 
-    // Pega o método
     $method = $_SERVER['REQUEST_METHOD'];
 
-    error_log(print_r($method, true));
-    error_log(print_r($body, true));
-    error_log(print_r($target_url, true));
+    $logger = wc_get_logger();
+    $context = ['source' => 'webhook-proxy'];
 
-    // Envia a requisição para o ambiente local
+    $requestData = [
+        "URL" => $target_url,
+        "Method" => $method,
+        "Body" => $body
+    ];
+
     $response = wp_remote_request($target_url, [
         'method'  => $method,
         'body'    => $body,
         'timeout' => 15,
     ]);
 
-    error_log(print_r($response, true));
-    // Finaliza sem processar mais nada
-    status_header(200);
-    exit;
+    if (is_wp_error($response)) {
+        $logger->error("Erro ao redirecionar para ngrok: " . $response->get_error_message(), $context);
+         status_header(502);
+    } 
+
+    //error_log(print_r($response, true));
+
+    $logBody = [
+        "Request" => $requestData,
+        "Response" => json_decode(wp_remote_retrieve_body($response))
+    ];
+    
+    $logger->info(json_encode($logBody, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES), $context);
+
+    wp_remote_retrieve_body($response);
 }
 
